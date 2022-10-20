dfea235ef82a2b10ed1daa474efc851f64d82025
riiduu <jelinsksk@gmail.com>
2022-10-20 21:35:32 +0300

    created some functionality

--- GUI.py
+++ GUI.py
@@ -1,10 +1,48 @@
+from curses import BUTTON1_CLICKED
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import random
 
+numbers = []
+
+def generateNumbers():
+    i = 0
+    while i < 3:
+        newNumber = random.randint(1, 100)
+        numbers.append(newNumber)
+        i += 1
+        
+def checkWinner(guess, winner):
+    if guess == winner:
+        print("You Won!")
+        #Add points
+    else:
+        print("You lost!")
+        #remove points
+        #if 0 then don't
+    
+def pickWinner():
+    winner = random.choice(numbers)
+    return winner
+    
+
+def clearNumbers():
+    numbers.clear()
+    
+def tryAgain():
+    main()
+    
+def game():
+    generateNumbers()
+    guess = MyGUI.button_1_click()
+    winner = pickWinner()
+    checkWinner(guess, winner)
+
 
 class MyGUI(QMainWindow):   
-        
+    points = 0
+    
+    
     def __init__(self): # initializes the object's attributes
         super(MyGUI, self).__init__() # super constructor
         uic.loadUi("GUI.ui", self) # loads the ui file
@@ -12,39 +50,25 @@ class MyGUI(QMainWindow):
         
         # gui functionality
         
-        self.button_2.clicked.connect(self.show_message)
+        self.button_1.clicked.connect(self.button_1_click)
     
     #functions
     
-    def show_message(self):
-        print("tavmamm")
-
-
-def checkWinner():
-    print("checkWinner")
     
-def pickWinner():
-    print("pickWinner")
-
-def clearNumbers():
-    print("clearNumbers")
-
-def generateNumbers(nums):
-    newNumber = random.randint(1, 100)
-    i = 0
-    while i < 3:
-        nums.append(newNumber)
-
-def main():
-    numbers = []
     
+    def button_1_click():
+        guess = numbers[0]
+        
+        return guess
+            
+    
+
+
+
+
+def main():    
     app = QApplication([])
     window = MyGUI()
-    
-    nums = generateNumbers(numbers) # adds 3 random numbers to the numbers array
-    print(nums)
-    
-    
     
     app.exec_()
 
